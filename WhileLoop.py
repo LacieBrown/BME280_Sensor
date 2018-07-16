1781719748295ebc9d0ba6889ff9d3007386c16b
Lacie Brown <lbrown16@nd.edu>
2018-07-16 14:27:01 -0400

    Changes made to both BME280 file: added a higher wait time so file save can happen changes made to loop structure

--- WhileLoop.py
+++ WhileLoop.py
@@ -10,27 +10,41 @@
 import BME280_Sensor as bme280
 import time
 import os
+import sys
+import datetime
+import json
+    
+false = KeyboardInterrupt
+file_name = open("SensorData.txt", "a+") 
+# Display File name data so the user can find the stored data on the local Pi
+print("The file name is ", file_name.name)
+print("The file is either closed or not ", file_name.closed)
+print("The opening mode of the file is ", file_name.mode)
 
 # Chip Data is displayed first and only once. 
 print('Chip Data')
 bme280.DisplayChipData()
-time.sleep(3)
 
 # Display the date and time for clean data collection
-
-time.sleep(2)
+time = time.time()
+dattime = datetime.datetime.fromtimestamp(time).strftime('Today is: ''%Y-%m-%d \nThe time is: ''%H:%M:%S')
+print(dattime)
 
 #Get users input to begin displaying sensor readings
-user_input = input("\nPress enter to begin and CTRL C to quit \n")
+user_input = raw_input("\nPress enter to begin and CTRL C to quit \n")
 
 # Output Sensor reading to screen in a loop until CTRL C is pressed
 try:
-    bme280.DisplayData()
-    print("\n")
-    print("\n")
+    while(True):
+        bme280.DisplayData()
+        with open("SensorData", "a+") as f:
+            json.dump(bme280.DisplayData(), f)
+        print("\n")
 # When CNTR C is pressed, finish by saving to a file 
-finally:
-    path = '/home/pi/BMESensorProject/SensorData.txt'
-    # Exit the program
-    sys.exit()
+except false:
+    print("\nInterrupted,  closing out")
+    f.close()
+
+# Exit the program
+sys.exit()
 
